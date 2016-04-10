94d3da32f37ba10552e851f0892b4b47843a632e
Brett Holman <brettholman91@gmail.com>
2016-04-10 13:03:42 -0700

    Changing UI for Samples.html

---
+++ Javascript/Solutions/PiToTheNth/PiToTheNth.js
@@ -0,0 +1,23 @@
+function submit() {
+    
+}
+
+function runTests() {
+    
+}
+
+function showC() {
+    var elem = document.getElementById("code");
+    elem.innerHTML = null;
+    elem.innerHTML = "<pre ng-include=\"\'../Solutions/PiToTheNth/c.html\'\"></pre>"
+    alert("done");
+    alert(elem.innerHTML);
+}
+
+function showJava() {
+    
+}
+
+function showNET() {
+    
+}
--- Javascript/script.js
+++ Javascript/script.js
@@ -1,3 +1,13 @@
+var elem = document.getElementById("playground");
+function getElem() {
+    elem = elem == null ? document.getElementById("playground") : elem;
+}
 function pi() {
-    document.getElementById("playground").src = "Playground/pi.html";
+    getElem();
+    elem.src = "Playground/pi.html";
+}
+
+function e() {
+    getElem();
+    elem.src = "Playground/default.html";
 }
